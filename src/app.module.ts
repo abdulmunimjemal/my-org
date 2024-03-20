3cc2fc56930e07138cdc5f6ca135e3ba061a226c
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-03-20 16:33:26 +0300

    Added env variable validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { DepartmentsController } from './departments/departments.controller';
 import { DepartmentsService } from './departments/departments.service';
 import { DepartmentsModule } from './departments/departments.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
-  imports: [DepartmentsModule],
+  imports: [
+    DepartmentsModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+  ],
   controllers: [DepartmentsController],
   providers: [DepartmentsService],
 })

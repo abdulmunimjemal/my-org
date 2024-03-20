97e82944a6fd3777b2ede1fa66152d84c483e158
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-03-20 16:01:45 +0300

    Removing unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DepartmentsController } from './departments/departments.controller';
 import { DepartmentsService } from './departments/departments.service';
 import { DepartmentsModule } from './departments/departments.module';
 
 @Module({
   imports: [DepartmentsModule],
-  controllers: [AppController, DepartmentsController],
-  providers: [AppService, DepartmentsService],
+  controllers: [DepartmentsController],
+  providers: [DepartmentsService],
 })
 export class AppModule {}

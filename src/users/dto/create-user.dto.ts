5f0d6e8ca8ecfea4d1e2a3c8ccfc912d311edd29
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2024-03-21 09:37:57 +0300

    Added User Dto

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,27 @@
+import {
+  IsEmail,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  readonly email;
+
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
+  readonly password;
+
+  @IsString()
+  readonly firstName;
+
+  @IsString()
+  @IsOptional()
+  readonly lastName;
+
+  @IsNumber()
+  @IsOptional()
+  readonly departmentId;
+}
---
+++ src/users/dto/update-user.dto.ts
@@ -0,0 +1,3 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDto } from './';
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
